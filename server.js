83a53f04e6a161e1f5cae0435b862370dc1c4683
N0364359 <michael.blydenburgh@libertymutual.com>
2019-05-12 20:38:21 -0400

    Initial Commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const path = require("path");
+const dev = process.env.NODE_ENV !== "production";
+const next = require("next");
+const pathMatch = require("path-match");
+
+const app = next({dev});
+const server = express();
+const route = pathMatch();
+server.use('/_next', express.static(path.join(__dirname, '.next')));
+
+server.get('/', (req, res) => app.render(req, res, '/'));
+server.get('*', (req, res) => handle(req, res));
+
+module.exports = server;
