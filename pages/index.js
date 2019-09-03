7c785d601e2fa54d85649ccce0323bd96609a45b
N0364359 <michael.blydenburgh@libertymutual.com>
2019-09-03 08:53:58 -0400

    Display basic query results

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,16 @@
-import React, { Component } from "react";
-import Styled from "styled-components";
-import ApolloClient, { gql } from "apollo-boost";
-// import { ApolloProvider } from "react-apollo";
+import React, { Component } from "react"
+import Styled from "styled-components"
+import ApolloClient, { gql } from "apollo-boost"
+// import { ApolloProvider } from "react-apollo"
+import Default from "../layouts/Default"
 
-import Default from "../layouts/Default";
+class Index extends Component {
 
-class Index extends Component{
+  state = {
+      claims: []
+  }
 
-  async componentDidMount(){
+  async componentDidMount() {
     const client = new ApolloClient({
       uri: `http://localhost:8080/graphql`
     })
@@ -15,18 +18,27 @@ class Index extends Component{
     const results = await client.query({
       query: gql`
         {
-          getAllClaims{
+          getAllClaims {
             claimId
+            lossDate
           }
         }
       `
     })
+
     // Query returns a data object with each query as a separate object
     const { data } = results
-    console.log("Return reults for the following queries:")
+    console.log("Return results for the following queries:")
     for(let key in data) {
-      console.log(key)
-      console.log(data[`${key}`])
+      if (data.hasOwnProperty(key)) {
+        console.log(key)
+        console.log(data[key])
+        let newClaims = data[key]
+        console.log(newClaims)
+        for (let claim in newClaims) {
+          this.setState({ claims: [...this.state.claims, newClaims[claim]] })
+        }
+      }
     }
   }
 
@@ -34,9 +46,16 @@ class Index extends Component{
     return(
       <Default>
         <h1>Claims</h1>
+        <ul>
+          {
+            this.state.claims.map((claim,index) => { return (
+              <li key={index}>Id:{claim.claimId}, Loss Date: {claim.lossDate}</li>
+            )})
+          }
+        </ul>
       </Default>
       )
   }
 }
 
-export default Index;
+export default Index
